64d132fc1e984acba806adbc28f5fd5432104617
Archie Balbin <arbalbin@gbox.ncf.edu.ph>
2025-11-25 14:42:10 +0800

    Update main.ts to enable CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.56.1:3000',
+      'https://your-frontend-domain-if-deployed.com'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3000);
   console.log('🚀 Server is up and running on http://localhost:3000');
-  console.log('Welcome to my NestJS API!');
 }
 bootstrap();
